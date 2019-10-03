a5d1f3f6c259753aff63be98d5e48c15c7d8e4ac
brad <bradford.sawadye@jembi.org>
2019-10-03 15:53:55 +0200

    add function for creating a joi schema
    The schema will be used for validating the incoming objects
    
    OHM-856

---
+++ middleware/validation.js
@@ -0,0 +1,81 @@
+'use strict'
+
+const Joi = require('@hapi/joi')
+const bahmniPatientInputValidations = require('../endpoints/bahmni/input-validation.json')
+
+// This method creates a joi vaidation schema. It returns null if the scheam creation fails
+const createJoiValidationSchema = (resourceName) => {
+  let validations
+  const schemaObject = {}
+
+  // TODO add more cases for other resources
+  switch (resourceName) {
+    case 'bahmniPatient':
+      validations = bahmniPatientInputValidations
+      break;
+
+    default:
+      break;
+  }
+
+  if (validations) {
+    Object.keys(validations).forEach(key => {
+      let rule = validations[`${key}`]
+      switch (rule.type) {
+        case 'string':
+          if (rule.required) {
+            schemaObject[`${key}`] = Joi.string().required()
+          } else if (rule.default) {
+            schemaObject[`${key}`] = Joi.string().default(rule.default)
+          } else {
+            schemaObject[`${key}`] = Joi.string()
+          } 
+          break;
+        
+        case 'number':
+          if (rule.required) {
+            schemaObject[`${key}`] = Joi.number().required()
+          } else if (rule.default) {
+            schemaObject[`${key}`] = Joi.number().default(rule.default)
+          } else {
+            schemaObject[`${key}`] = Joi.number()
+          } 
+          break;
+        
+        case 'boolean':
+          if (rule.required) {
+            schemaObject[`${key}`] = Joi.boolean().required()
+          } else if (rule.default) {
+            schemaObject[`${key}`] = Joi.boolean().default(rule.default)
+          } else {
+            schemaObject[`${key}`] = Joi.boolean()
+          }
+          break;
+        
+        case 'array':
+          if (rule.required) {
+            schemaObject[`${key}`] = Joi.array().required()
+          } else if (rule.default) {
+            schemaObject[`${key}`] = Joi.array().default(rule.default)
+          } else {
+            schemaObject[`${key}`] = Joi.array()
+          }
+          break;
+
+        default:
+
+          break;
+      }
+    })
+
+    if (schemaObject) return Joi.object(schemaObject)
+    return null
+  }
+
+  return null
+}
+
+if (process.env.NODE_ENV == "test") {
+  exports.createJoiValidationSchema = createJoiValidationSchema
+}
+
