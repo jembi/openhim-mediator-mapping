3a4681c37624966a8a17c7e706a579858a762b38
MattyJ007 <matthew.dickie@jembi.org>
2019-10-10 17:03:12 +0200

    Add constants.json files to routes
    This file will enable users to specify fields and their mapping that are
    constant in the output object. This is useful especially when the output
    object requires fields that aren't represented in the input.
    
    OHM-880

--- src/constants.js
+++ src/constants.js
@@ -3,8 +3,15 @@
 const inputMeta = 'meta.json'
 const inputValidation = 'input-validation.json'
 const inputMapping = 'input-mapping.json'
+const inputConstants = 'constants.json'
 
-exports.expectedEndpointsDirectories = [inputMeta, inputValidation, inputMapping]
+exports.expectedEndpointsDirectories = [
+  inputMeta,
+  inputValidation,
+  inputMapping,
+  inputConstants
+]
 exports.inputMeta = inputMeta
 exports.inputValidation = inputValidation
 exports.inputMapping = inputMapping
+exports.inputConstants = inputConstants
