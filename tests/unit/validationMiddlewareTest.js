1ae7c81f54525ea4342e2d8103516582f5d38b45
brad <bradford.sawadye@jembi.org>
2019-10-11 16:38:04 +0200

    modify the validation tests
    The validation functionality has been changed to make use of the ajv library resulting in the tests breaking
    
    OHM-876

--- tests/unit/validationMiddlewareTest.js
+++ tests/unit/validationMiddlewareTest.js
@@ -1,57 +1,11 @@
 'use strict'
 
 const tape = require('tape')
-const Joi = require('@hapi/joi')
-const {
-  createValidationSchema,
-  performValidation
-} = require('../../src/middleware/validator')
+const {performValidation} = require('../../src/middleware/validator')
 
 tape.test('Validation Middleware', t => {
-  t.test('createValidationSchema()', t => {
-    t.test('should throw when resource is not supplied', t => {
-      t.throws(
-        createValidationSchema,
-        new Error(`Error: No validation rules supplied`)
-      )
-      t.end()
-    })
-
-    t.test('should throw when resource type is not supported', t => {
-      const validationMap = {
-        directory: 'unsupported'
-      }
-
-      t.throws(
-        () => createValidationSchema(validationMap),
-        new Error(`Validation rule type is not supported:`)
-      )
-      t.end()
-    })
-
-    t.test('should return a schema object', t => {
-      const validationMap = {
-        directory: {
-          type: 'string',
-          required: true
-        }
-      }
-
-      const result = createValidationSchema(validationMap)
-
-      t.notEqual(result, null)
-      t.end()
-    })
-  })
-
   t.test('performValidation()', t => {
-    const joiSchema = Joi.object({
-      name: Joi.string().required(),
-      surname: Joi.string(),
-      age: Joi.number().required()
-    })
-
-    t.test('should throw for missing required number fields', t => {
+    t.test('should throw when validation schema is falsy', t => {
       const ctx = {
         request: {
           body: {
@@ -60,44 +14,90 @@ tape.test('Validation Middleware', t => {
           }
         }
       }
+      const schema = null
 
       t.throws(
-        () => performValidation(ctx, joiSchema),
-        `Validation execution failed: "age" is required`
+        () => performValidation(ctx, schema),
+        /No validation rules supplied/
       )
       t.end()
     })
 
-    t.test('should throw for missing required string fields', t => {
+    t.test('should throw when request body is not supplied', t => {
+      const ctx = {
+        request: {}
+      }
+      const schema = {}
+
+      t.throws(() => performValidation(ctx, schema), /Invalid request body/)
+      t.end()
+    })
+
+    t.test('should throw when request is body not valid', t => {
       const ctx = {
         request: {
           body: {
-            surname: 'durden',
-            age: 33
+            surname: 'durden'
           }
         }
       }
+      const schema = {
+        type: 'object',
+        properties: {
+          name: {type: 'string'},
+          surname: {type: 'string'}
+        },
+        required: ['name']
+      }
 
       t.throws(
-        () => performValidation(ctx, joiSchema),
-        `Validation execution failed: "name" is required`
+        () => performValidation(ctx, schema),
+        /Validation execution failed/
       )
       t.end()
     })
 
-    t.test('should not throw when validation succeeds', t => {
+    t.test('should validate', t => {
       const ctx = {
         request: {
           body: {
-            name: 'tyler',
-            surname: 'durden',
-            age: 21
+            name: 'drake',
+            surname: 'durden'
           }
         }
       }
+      const schema = {
+        type: 'object',
+        properties: {
+          name: {type: 'string'},
+          surname: {type: 'string'}
+        },
+        required: ['name']
+      }
 
-      performValidation(ctx, joiSchema)
+      t.doesNotThrow(() => performValidation(ctx, schema))
+      t.end()
+    })
 
+    t.test('should validate when a property has a value of null', t => {
+      const ctx = {
+        request: {
+          body: {
+            name: 'drake',
+            surname: null
+          }
+        }
+      }
+      const schema = {
+        type: 'object',
+        properties: {
+          name: {type: 'string'},
+          surname: {type: 'string', nullable: true}
+        },
+        required: ['name']
+      }
+
+      t.doesNotThrow(() => performValidation(ctx, schema))
       t.end()
     })
   })
