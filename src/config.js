08c36b0bb1471b556bab2a3daea8a1132c9c4d32
brad <bradford.sawadye@jembi.org>
2019-10-14 15:25:59 +0200

    refactor the logic that sets the configs
    This is just to make the code neat
    
    OHM-876

--- src/config.js
+++ src/config.js
@@ -10,6 +10,6 @@ exports.getConfig = function() {
       password: process.env.OPENHIM_PASSWORD || 'openhim-password',
       trustSelfSigned: process.env.TRUST_SELF_SIGNED === 'true'
     }),
-    nullable: !(process.env.ACCEPT_NULL_VALUES === 'false')
+    nullable: process.env.ACCEPT_NULL_VALUES !== 'false'
   })
 }
