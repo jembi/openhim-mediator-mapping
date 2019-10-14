2e0b61d608e28895df6c36427b86f2a83cc9a510
brad <bradford.sawadye@jembi.org>
2019-10-14 15:11:49 +0200

    remove line calling function that no longer exists
    The validation middleware has been configured and when master was merged, a statement, in which a function that no longer exists is called, was added back in. The statement is removed in this commit
    
    OHM-876

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -27,13 +27,12 @@ const performValidation = (ctx, schema) => {
 
 exports.validateInput = schema => async (ctx, next) => {
   try {
-    const schema = createValidationSchema(validationMap)
     performValidation(ctx, schema)
     logger.info('Successfully validated user input')
   } catch (error) {
     ctx.status = 400
     ctx.type = 'json'
-    ctx.body = JSON.stringify({ error: error.message })
+    ctx.body = JSON.stringify({error: error.message})
     return logger.error(error.message)
   }
 
