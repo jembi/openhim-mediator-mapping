d2ab9d1ecbff310b25aa8328bc4c30645e99446a
Martin Brocker <martin.brocker@jembi.org>
2019-10-15 08:58:09 +0200

    Ensure input config contains constants before trying to join it into the mapping schema
    This was throwing an error when its not defined
    
    OHM-840

---
+++ src/middleware/parser.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const xml2js = require('xml2js')
+const KoaBodyParser = require('@viweei/koa-body-parser')
+
+const logger = require('../logger')
+const xmlBuilder = new xml2js.Builder()
+
+const parseOutgoingBody = (ctx, outputFormat) => {
+  if (outputFormat === 'XML') {
+    try {
+      ctx.body = xmlBuilder.buildObject(ctx.body)
+      ctx.set('Content-Type', 'application/xml')
+      logger.info(`Parsed outgoing body in ${outputFormat} format`)
+    } catch (error) {
+      throw new Error(`Parsing outgoing body failed: ${error.message}`)
+    }
+  }
+}
+
+const parseIncomingBody = async (ctx, inputFormat, next) => {
+  // parse incoming body
+  // KoaBodyParser executed the next() callback to allow the other middleware to continue before coming back here
+  if (['JSON', 'XML'].includes(inputFormat)) {
+    const options = {
+      limit: '1mb',
+      xmlOptions: {
+        trim: true,
+        explicitRoot: false,
+        explicitArray: false
+      }
+    }
+    
+    try {
+      await KoaBodyParser(options)(ctx, next)
+      logger.info(`Parsed incoming body into JSON format for processing`)
+    } catch (error) {
+      throw new Error(`Parsing incoming body failed: ${error.message}`)
+    }
+  } else {
+    throw new Error(`transformation method "${inputFormat}" not yet supported`)
+  }
+}
+
+exports.parsePayloadMiddleware = metaData => async (ctx, next) => {
+  try {
+    // parse incoming body
+    await parseIncomingBody(ctx, metaData.transformation.input, next)
+
+    // wait for middleware to bubble up before running the below method
+
+    // parse outgoing body
+    parseOutgoingBody(ctx, metaData.transformation.output)
+  } catch (error) {
+    ctx.status = 400
+    ctx.type = 'json'
+    ctx.body = JSON.stringify({ error: error.message })
+    return logger.error(error.message)
+  }
+}
