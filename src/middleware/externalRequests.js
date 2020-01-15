ec3ea94e06206e2822808e261dc12d7512799ecd
MattyJ007 <matthew.dickie@jembi.org>
2020-01-15 08:50:33 +0200

    Initial request middleware code
    This logic creates an array of requests with details defined in the
    meta.json. The requests are asynchronous but they need to also be
    assigned a predefined ID.
    This code doesn't work as the promise is not resolving properly.
    
    OHIE-294

--- src/middleware/externalRequests.js
+++ src/middleware/externalRequests.js
@@ -1,8 +1,39 @@
 'use strict'
 
+const fetch = require('node-fetch')
+
 const logger = require('../logger')
 
+function checkStatus(res) {
+  if (res.ok) {
+    return res
+  } else {
+    throw new Error('Non-2xx Response status')
+  }
+}
+
 exports.requestsMiddleware = () => async (ctx, next) => {
-  logger.info('Meta Data: ', ctx.state.metaData)
+  const requests = ctx.state.metaData.requests
+  if (requests && requests.lookup && requests.lookup.length) {
+    const responseData = requests.lookup.map(requestDetails => {
+      logger.debug(requestDetails.url)
+      return fetch(requestDetails.url)
+        .then(checkStatus)
+        .then(res => {
+          return {[requestDetails.id]: res.json()}
+        })
+        .catch(err => {
+          logger.error(err)
+        })
+    })
+
+    await Promise.all(responseData)
+      .then(result => {
+        logger.info(`Promise all data: ${result}`)
+      })
+      .catch(err => {
+        logger.error(err)
+      })
+  }
   await next()
 }
