44a17b650477b631796e904f06a6e275c2cc0c64
MattyJ007 <matthew.dickie@jembi.org>
2020-01-16 16:03:18 +0200

    Refactor request middleware to be more testable
    The code is now split out better to keep function responsibility
    singular. These split out functions can now also be tested properly.
    
    OHIE-294

--- src/middleware/externalRequests.js
+++ src/middleware/externalRequests.js
@@ -1,61 +1,70 @@
 'use strict'
 
 const axios = require('axios')
-const queryString = require('querystring')
 
 const logger = require('../logger')
 
-function prepareRequestConfig(requestDetails, query) {
-  const requestOptions = {
-    url: `${requestDetails.url}?${queryString.stringify(query)}`,
-    method: requestDetails.method || 'get'
-  }
-  if (requestDetails && requestDetails.headers != null) {
-    requestOptions.headers = requestDetails.headers
-  }
-  return requestOptions
+const performRequests = requests => {
+  return requests.map(requestDetails => {
+    return axios(prepareRequestConfig(requestDetails))
+      .then(res => {
+        // Assign any data received from the response to the assigned id in the context
+        return {[requestDetails.id]: res.data}
+      })
+      .catch(error => {
+        logger.error(
+          `Failed Request Config ${JSON.stringify(error.config, null, 2)}`
+        )
+        if (error.response) {
+          throw new Error(
+            `Incorrect status code ${error.response.status}. ${error.response.data.message}`
+          )
+        } else if (error.request) {
+          throw new Error(
+            `No response from lookup '${requestDetails.id}'. ${error.request}`
+          )
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          throw new Error(`Unhandled Error: ${error.message}`)
+        }
+      })
+  })
 }
 
-exports.requestsMiddleware = () => async (ctx, next) => {
-  const requests = ctx.state.metaData.requests
+const prepareLookupRequests = ctx => {
+  const requests = Object.assign({}, ctx.state.metaData.requests)
   if (requests && requests.lookup && requests.lookup.length > 0) {
-    ctx.externalRequest = {}
-    const responseData = requests.lookup.map(requestDetails => {
-      return axios(prepareRequestConfig(requestDetails, ctx.request.query))
-        .then(res => {
-          // Assign any data received from the response to the assigned id in the context
-          ctx.externalRequest[requestDetails.id] = res.data
-        })
-        .catch(error => {
-          logger.error(
-            `${ctx.state.metaData.name} (${
-              ctx.state.uuid
-            }): Failed Request Config ${JSON.stringify(error.config, null, 2)}`
-          )
-          if (error.response) {
-            throw new Error(
-              `Incorrect status code ${error.response.status}. ${error.response.data.message}`
-            )
-          } else if (error.request) {
-            throw new Error(
-              `No response from lookup '${requestDetails.id}'. ${error.request}`
-            )
-          } else {
-            // Something happened in setting up the request that triggered an Error
-            throw new Error(`Unhandled Error: ${error.message}`)
-          }
-        })
-    })
+    const responseData = performRequests(requests.lookup)
 
-    await Promise.all(responseData)
-      .then(() => {
-        logger.trace(
-          `All requests resolved: ${JSON.stringify(ctx.externalRequest)}`
+    return Promise.all(responseData)
+      .then(data => {
+        logger.info(
+          `${ctx.state.metaData.name} (${ctx.state.uuid}): Successfully performed request/s`
         )
+        ctx.externalRequests = data
       })
       .catch(err => {
         throw new Error(`Rejected Promise: ${err}`)
       })
+  } else {
+    logger.debug(
+      `${ctx.state.metaData.name} (${ctx.state.uuid}): No request/s to make`
+    )
   }
+}
+
+const prepareRequestConfig = requestDetails => {
+  const requestOptions = Object.assign({}, requestDetails.config)
+  // This step is separated out as in future the URL contained within the config
+  // can be manipulated to add URL parameters taken from the body of an incoming request
+  return requestOptions
+}
+
+exports.requestsMiddleware = () => async (ctx, next) => {
+  await prepareLookupRequests(ctx)
   await next()
+}
+
+if (process.env.NODE_ENV == 'test') {
+  exports.prepareRequestConfig = prepareRequestConfig
 }
