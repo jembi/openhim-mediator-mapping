e43a09f4c34487ec6c25e489e49676f15dcddab8
brad <bradford.sawadye@jembi.org>
2020-01-21 12:34:02 +0200

    modify the tests and add another test
    The signature of the function being tested has been changed so the tests had to be changed as well
    
    OHIE-297

--- tests/unit/externalRequestsTest.js
+++ tests/unit/externalRequestsTest.js
@@ -5,14 +5,16 @@ const {createOrchestration} = require('../../src/middleware/externalRequests')
 
 tap.test('External Requests', {autoend: true}, t => {
   t.test('createOrchestration', {autoend: true}, t => {
-    t.test("should fail when request's url or method is not supplied", t => {
+    t.test("should fail when request's url or method are falsy", t => {
       t.plan(1)
 
-      const url = null
-      const method = null
+      const request = {
+        url: null,
+        method: null
+      }
 
       try {
-        createOrchestration(url, method, null, null, null, null, null, null)
+        createOrchestration(request, null, null, null, null, null)
       } catch (error) {
         t.equals(
           error.message,
@@ -21,14 +23,17 @@ tap.test('External Requests', {autoend: true}, t => {
       }
     })
 
-    t.test("should fail when request's timestamp is not supplied", t => {
+    t.test("should fail when request's timestamp is falsy", t => {
       t.plan(1)
 
-      const url = 'http://localhost'
-      const method = 'PUT'
+      const request = {
+        url: 'http://localhost',
+        method: 'PUT'
+      }
+      const reqTimestamp = null
 
       try {
-        createOrchestration(url, method, null, null, null, null, null, null)
+        createOrchestration(request, null, null, reqTimestamp, null)
       } catch (error) {
         t.equals(error.message, 'Orchestration request timestamp not supplied')
       }
@@ -37,24 +42,75 @@ tap.test('External Requests', {autoend: true}, t => {
     t.test('should fail when orchestration name is not supplied', t => {
       t.plan(1)
 
-      const url = 'http://localhost'
-      const method = 'PUT'
+      const request = {
+        url: 'http://localhost',
+        method: 'PUT',
+        id: null
+      }
       const reqTimestamp = Date.now()
 
       try {
-        createOrchestration(
-          url,
-          method,
-          null,
-          null,
-          null,
-          reqTimestamp,
-          null,
-          null
-        )
+        createOrchestration(request, null, null, reqTimestamp, null, null)
       } catch (error) {
         t.equals(error.message, 'Orchestration name not supplied')
       }
+    })
+
+    t.test('should create orchestration', t => {
+      const headers = {'Content-Type': 'application/json'}
+      const request = {
+        url: 'http://localhost:8000/patient/?name=brainman',
+        method: 'PUT',
+        id: 'Patient',
+        headers
+      }
+      const reqTimestamp = Date.now()
+
+      const requestBody = {surname: 'raze'}
+      const response = {
+        body: {
+          name: 'brainman',
+          surname: 'raze'
+        },
+        status: 200,
+        headers
+      }
+      const responseTimestamp = Date.now()
+      const name = 'Patient'
+
+      const expectedOrch = {
+        request: {
+          host: 'localhost',
+          port: '8000',
+          path: '/patient/',
+          timestamp: reqTimestamp,
+          method: 'PUT',
+          queryString: 'name=brainman',
+          headers,
+          body: requestBody
+        },
+        response: {
+          timestamp: responseTimestamp,
+          status: 200,
+          headers: headers,
+          body: {
+            name: 'brainman',
+            surname: 'raze'
+          }
+        },
+        name
+      }
+
+      const orchestration = createOrchestration(
+        request,
+        requestBody,
+        response,
+        reqTimestamp,
+        responseTimestamp
+      )
+
+      t.deepEqual(expectedOrch, orchestration)
+      t.end()
     })
   })
 })
