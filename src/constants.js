382c88727e7ba5d5b88372c2ed4f2d14da9e5345
MattyJ007 <matthew.dickie@jembi.org>
2020-04-08 09:38:22 +0200

    Remove unused constants from constants file
    The constants file was a reference for the mapper config file names.
    Since we are using mongo db now we no longer need file name references.
    
    OHIE-375

--- src/constants.js
+++ src/constants.js
@@ -1,18 +1,3 @@
 'use strict'
 
-const inputMeta = 'meta.json'
-const inputValidation = 'input-validation.json'
-const inputMapping = 'input-mapping.json'
-const inputConstants = 'constants.json'
-
-exports.expectedEndpointsDirectories = [
-  inputMeta,
-  inputValidation,
-  inputMapping,
-  inputConstants
-]
-exports.inputMeta = inputMeta
-exports.inputValidation = inputValidation
-exports.inputMapping = inputMapping
-exports.inputConstants = inputConstants
 exports.ALLOWED_CONTENT_TYPES = ['JSON', 'XML']
