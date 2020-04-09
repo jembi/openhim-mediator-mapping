429180dc10dda9460d1cdd1308bd58e85d90999a
MattyJ007 <matthew.dickie@jembi.org>
2020-04-09 17:01:14 +0200

    Make the dynamic enpoint functionality configurable
    This is helpful for running tests.
    The change listeners can cause issues during testing as their behaviour
    can be difficult to control in the test environment (specifically on
    Travis). Therefore when the caches need to be updated this can be done
    manually.
    
    OHIE-375

--- src/db/main.js
+++ src/db/main.js
@@ -4,33 +4,57 @@ const mongoose = require('mongoose')
 
 const logger = require('../logger')
 
+const {dynamicEndpoints} = require('../config').getConfig()
 const {
   removeEventListeners,
   setupEventListeners
 } = require('./services/endpoints/cache')
 
 exports.open = async mongoUrl => {
-  try {
-    await mongoose.connect(mongoUrl, {
+  await mongoose
+    .connect(mongoUrl, {
+      poolSize: 20,
+      useCreateIndex: true,
       useNewUrlParser: true,
       useFindAndModify: false
     })
-    logger.info(`Connected to mongo on ${mongoUrl}`)
-  } catch (err) {
-    logger.error(`Failed to connect to mongo. Caused by: ${err.message}`, err)
-    throw err
-  }
+    .then(() => {
+      logger.info(`Connected to mongo on ${mongoUrl}`)
+      if (dynamicEndpoints) {
+        setupEventListeners()
+      }
+    })
+    .catch(error => {
+      logger.error(
+        `Failed to connect to mongo. Caused by: ${error.message}`,
+        error
+      )
+      throw error
+    })
 }
 
 exports.close = async () => {
-  try {
-    await mongoose.connection.close()
-    logger.info(`Closed DB connection.`)
-  } catch (err) {
-    logger.error(
-      `Failed to close DB connection. Caused by: ${err.message}`,
-      err
-    )
-    throw err
+  if (dynamicEndpoints) {
+    removeEventListeners()
   }
+  await mongoose
+    .disconnect()
+    .then(() => {
+      logger.info(`Closed DB connection.`)
+    })
+    .catch(error => {
+      logger.error(
+        `Failed to close DB connection. Caused by: ${error.message}`,
+        error
+      )
+      throw error
+    })
 }
+
+// TODO: Add DB connection listeners to Mongoose Connection.
+// This will be useful for reconnecting to Mongo after an error
+// and re-establishing the dynamic endpoint functionality without
+// having to restart the Mapping mediator (resulting in downtime)
+// These Connection Listeners would work to re-enable the Event
+// Change listeners
+// https://mongoosejs.com/docs/connections.html#connection-events
