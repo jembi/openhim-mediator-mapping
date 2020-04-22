5dac152b891541c4060799d35f061d7849a1946b
Martin Brocker <martin.brocker@jembi.org>
2020-04-22 12:54:58 +0200

    Update tests/unit/parser.js
    formatting XML document for better readability
    
    Co-Authored-By: Matt <matthew.dickie@jembi.org>

--- tests/unit/parser.js
+++ tests/unit/parser.js
@@ -430,12 +430,14 @@ tap.test('Parser', {autoend: true}, t => {
 
       t.equals(
         ctx.body,
-        '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n' +
-          '<root>\n' +
-          '  <Name>Jet</Name>\n' +
-          '  <Surname>Li</Surname>\n' +
-          '  <Email>jet@openhim.org</Email>\n' +
+        [
+          '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>',
+          '<root>',
+          '  <Name>Jet</Name>',
+          '  <Surname>Li</Surname>',
+          '  <Email>jet@openhim.org</Email>',
           '</root>'
+        ].join('\n')
       )
 
       t.pass()
