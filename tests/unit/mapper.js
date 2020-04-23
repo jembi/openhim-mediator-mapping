02ff7a1ff1b3c2ec8061a199358aaea3a5ae0761
brad <bradford.sawadye@jembi.org>
2020-04-23 13:32:12 +0200

    add logic to enable validation without mapping
    The mapping mediator can be used for validation only. This is done by not defining a mapping schema. The body sent through woll be validated and then sent back. If lookup requests are done, the results will also be sent back
    
    OHM-998

--- tests/unit/mapper.js
+++ tests/unit/mapper.js
@@ -8,25 +8,65 @@ const createMappedObject = mapper.__get__('createMappedObject')
 
 tap.test('Mapper', {autoend: true}, t => {
   t.test('createMappedObject()', {autoend: true}, t => {
-    t.test('should set the inputMapping if its undefined', t => {
-      const ctx = {
-        state: {
-          uuid: 'randomUidForRequest',
-          metaData: {
-            name: 'Testing endpoint'
+    t.test(
+      'should do no mapping if mapping schema is undefined and return the object sent and lookups done',
+      t => {
+        const ctx = {
+          state: {
+            uuid: 'randomUidForRequest',
+            metaData: {
+              name: 'Testing endpoint'
+            },
+            allData: {
+              requestBody: {
+                name: 'Timothy'
+              },
+              lookupRequests: {
+                surname: 'Wesley'
+              }
+            }
           },
-          allData: {}
-        },
-        request: {
-          body: {}
+          request: {
+            body: {}
+          }
         }
+
+        createMappedObject(ctx)
+
+        t.deepEqual(ctx.body, {
+          requestBody: ctx.state.allData.requestBody,
+          lookupRequests: ctx.state.allData.lookupRequests
+        })
+        t.end()
       }
+    )
 
-      createMappedObject(ctx)
+    t.test(
+      'should do no mapping if mapping schema is undefined and return the object sent',
+      t => {
+        const ctx = {
+          state: {
+            uuid: 'randomUidForRequest',
+            metaData: {
+              name: 'Testing endpoint'
+            },
+            allData: {
+              requestBody: {
+                name: 'Timothy'
+              }
+            }
+          },
+          request: {
+            body: {}
+          }
+        }
 
-      t.deepEqual(ctx.state.metaData.inputMapping, {})
-      t.end()
-    })
+        createMappedObject(ctx)
+
+        t.deepEqual(ctx.body, ctx.state.allData.requestBody)
+        t.end()
+      }
+    )
 
     t.test(
       'should create an object based on mapping (from request body)',
