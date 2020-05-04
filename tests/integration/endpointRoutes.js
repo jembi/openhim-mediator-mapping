f6ada310a5390ebdb72c53236dc543726800afc4
brad <bradford.sawadye@jembi.org>
2020-05-04 17:14:51 +0200

    add integration tests for the endpoint routes
    These are the integration tests of the routes that are used for creating, reading, updating and deleting the mapping mediator routes
    
    OHM-1014

---
+++ tests/integration/endpointRoutes.js
@@ -0,0 +1,352 @@
+'use strict'
+
+const request = require('supertest')
+const tap = require('tap')
+
+const {withTestMapperServer} = require('../utils')
+
+const port = 13008
+process.env.MONGO_URL = 'mongodb://localhost:27017/endpoints'
+
+tap.test(
+  'Endpoint Routes',
+  withTestMapperServer(port, async t => {
+    t.test('create endpoint route', {autoend: true}, t => {
+      t.test('should return 400 when the request body is invalid', async t => {
+        const testEndpoint = {}
+
+        await request(`http://localhost:${port}`)
+          .post('/endpoints')
+          .send(testEndpoint)
+          .set('Content-Type', 'application/json')
+          .expect(400)
+
+        t.end()
+      })
+
+      t.test('should create endpoint', async t => {
+        const testEndpoint = {
+          name: 'Test Endpoint',
+          endpoint: {
+            pattern: '/test'
+          },
+          transformation: {
+            input: 'XML',
+            output: 'XML'
+          }
+        }
+
+        const result = await request(`http://localhost:${port}`)
+          .post('/endpoints')
+          .send(testEndpoint)
+          .set('Content-Type', 'application/json')
+          .expect(201)
+
+        t.equals(result.body.name, testEndpoint.name)
+        t.equals(result.body.pattern, testEndpoint.pattern)
+        t.end()
+      })
+    })
+
+    t.test('read endpoint route', {autoend: true}, t => {
+      t.test(
+        'should return a 400 response when endpoint id is invalid',
+        async t => {
+          const endpointId = '123'
+
+          const result = await request(`http://localhost:${port}`)
+            .get(`/endpoints/${endpointId}`)
+            .expect(400)
+
+          t.match(result.body.error, /Cast to ObjectId failed/)
+          t.end()
+        }
+      )
+
+      t.test(
+        'should return a 404 response when endpoint does not exist',
+        async t => {
+          const endpointId = '5db1e8e9d074237a356b36d1'
+
+          const result = await request(`http://localhost:${port}`)
+            .get(`/endpoints/${endpointId}`)
+            .expect(404)
+
+          t.equals(
+            result.body.error,
+            `Endpoint with id ${endpointId} does not exist`
+          )
+          t.end()
+        }
+      )
+
+      t.test('should create endpoint', async t => {
+        const testEndpoint = {
+          name: 'Test Endpoint 1',
+          endpoint: {
+            pattern: '/testSuccess1'
+          },
+          transformation: {
+            input: 'XML',
+            output: 'XML'
+          }
+        }
+
+        const endpoint = await request(`http://localhost:${port}`)
+          .post('/endpoints')
+          .send(testEndpoint)
+          .set('Content-Type', 'application/json')
+          .expect(201)
+
+        const result = await request(`http://localhost:${port}`)
+          .get(`/endpoints/${endpoint.body._id}`)
+          .expect(200)
+
+        t.equals(result.body.name, testEndpoint.name)
+        t.equals(result.body.pattern, testEndpoint.pattern)
+        t.end()
+      })
+    })
+
+    t.test('read all endpoints route', {autoend: true}, t => {
+      t.test('should get endpoints', async t => {
+        const testEndpoint = {
+          name: 'Test Endpoint 2',
+          endpoint: {
+            pattern: '/testSuccess2'
+          },
+          transformation: {
+            input: 'XML',
+            output: 'XML'
+          }
+        }
+
+        const testEndpoint2 = {
+          name: 'Test Endpoint 3',
+          endpoint: {
+            pattern: '/testSuccess3'
+          },
+          transformation: {
+            input: 'XML',
+            output: 'XML'
+          }
+        }
+
+        await request(`http://localhost:${port}`)
+          .post('/endpoints')
+          .send(testEndpoint)
+          .set('Content-Type', 'application/json')
+          .expect(201)
+
+        await request(`http://localhost:${port}`)
+          .post('/endpoints')
+          .send(testEndpoint2)
+          .set('Content-Type', 'application/json')
+          .expect(201)
+
+        const result = await request(`http://localhost:${port}`)
+          .get(`/endpoints`)
+          .expect(200)
+
+        t.ok(result.body.length)
+        t.end()
+      })
+
+      t.test('should get endpoints based on query', async t => {
+        const testEndpoint = {
+          name: 'Test Endpoint 4',
+          endpoint: {
+            pattern: '/testSuccess4'
+          },
+          transformation: {
+            input: 'XML',
+            output: 'XML'
+          }
+        }
+
+        const testEndpoint2 = {
+          name: 'Test Endpoint 5',
+          endpoint: {
+            pattern: '/testSuccess5'
+          },
+          transformation: {
+            input: 'XML',
+            output: 'XML'
+          }
+        }
+
+        await request(`http://localhost:${port}`)
+          .post('/endpoints')
+          .send(testEndpoint2)
+          .set('Content-Type', 'application/json')
+          .expect(201)
+
+        const endpoint = await request(`http://localhost:${port}`)
+          .post('/endpoints')
+          .send(testEndpoint)
+          .set('Content-Type', 'application/json')
+          .expect(201)
+
+        const result = await request(`http://localhost:${port}`)
+          .get(`/endpoints?_id=${endpoint.body._id}`)
+          .expect(200)
+
+        t.equals(result.body.length, 1)
+        t.equals(result.body[0].name, testEndpoint.name)
+        t.equals(result.body[0].pattern, testEndpoint.pattern)
+        t.end()
+      })
+    })
+
+    t.test('update endpoint route', {autoend: true}, t => {
+      t.test(
+        'should return a 400 response status when endpoint id is invalid',
+        async t => {
+          const update = {}
+          const endpointId = 'invalid'
+
+          await request(`http://localhost:${port}`)
+            .put(`/endpoints/${endpointId}`)
+            .send(update)
+            .set('Content-Type', 'application/json')
+            .expect(400)
+
+          t.end()
+        }
+      )
+
+      t.test(
+        'should return a 400 response status when update object is empty',
+        async t => {
+          const update = {}
+          const endpointId = 'invalid'
+
+          await request(`http://localhost:${port}`)
+            .put(`/endpoints/${endpointId}`)
+            .send(update)
+            .set('Content-Type', 'application/json')
+            .expect(400)
+
+          t.end()
+        }
+      )
+
+      t.test(
+        'should return a 404 response status when endpoint does not exist',
+        async t => {
+          const update = {
+            name: 'New name'
+          }
+          const endpointId = '5db1e8e9d074237a356b36d1'
+
+          const result = await request(`http://localhost:${port}`)
+            .put(`/endpoints/${endpointId}`)
+            .send(update)
+            .set('Content-Type', 'application/json')
+            .expect(404)
+
+          t.equals(
+            result.body.error,
+            `Endpoint with id ${endpointId} does not exist`
+          )
+          t.end()
+        }
+      )
+
+      t.test('should update the endpoint', async t => {
+        const testEndpoint = {
+          name: 'Test Endpoint 6',
+          endpoint: {
+            pattern: '/testSuccess6'
+          },
+          transformation: {
+            input: 'XML',
+            output: 'XML'
+          }
+        }
+        const update = {
+          transformation: {
+            input: 'JSON',
+            output: 'JSON'
+          }
+        }
+
+        const endpoint = await request(`http://localhost:${port}`)
+          .post('/endpoints')
+          .send(testEndpoint)
+          .set('Content-Type', 'application/json')
+          .expect(201)
+
+        const result = await request(`http://localhost:${port}`)
+          .put(`/endpoints/${endpoint.body._id}`)
+          .send(update)
+          .set('Content-Type', 'application/json')
+          .expect(200)
+
+        t.deepEqual(result.body.transformation, update.transformation)
+        t.end()
+      })
+    })
+
+    t.test('delete endpoint', {autoend: true}, t => {
+      t.test(
+        'should return a 400 response status when endpoint id is invalid',
+        async t => {
+          const endpointId = 'invalid'
+
+          await request(`http://localhost:${port}`)
+            .delete(`/endpoints/${endpointId}`)
+            .expect(400)
+
+          t.end()
+        }
+      )
+
+      t.test(
+        'should return a 404 response status when endpoint does not exist',
+        async t => {
+          const endpointId = '5db1e8e9d074237a356b36d1'
+
+          const result = await request(`http://localhost:${port}`)
+            .delete(`/endpoints/${endpointId}`)
+            .expect(404)
+
+          t.equals(
+            result.body.error,
+            `Endpoint with id '${endpointId}' does not exist`
+          )
+          t.end()
+        }
+      )
+
+      t.test('should delete the endpoint', async t => {
+        const testEndpoint = {
+          name: 'Test Endpoint 7',
+          endpoint: {
+            pattern: '/testSuccess7'
+          },
+          transformation: {
+            input: 'XML',
+            output: 'XML'
+          }
+        }
+
+        const endpoint = await request(`http://localhost:${port}`)
+          .post('/endpoints')
+          .send(testEndpoint)
+          .set('Content-Type', 'application/json')
+          .expect(201)
+
+        const result = await request(`http://localhost:${port}`)
+          .delete(`/endpoints/${endpoint.body._id}`)
+          .expect(200)
+
+        t.deepEqual(
+          result.body.message,
+          `Endpoint with id '${endpoint.body._id}' deleted`
+        )
+        t.end()
+      })
+    })
+  })
+)
