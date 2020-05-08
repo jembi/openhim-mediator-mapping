f984b7a77a6c6e4024d66aab4a08a28dc05161e7
brad <bradford.sawadye@jembi.org>
2020-05-08 13:43:37 +0200

    refactor the function that creates the routes
    This is so we remove duplication of code that was there
    
    OHIE=386

--- src/routes.js
+++ src/routes.js
@@ -6,31 +6,23 @@ const {parseBodyMiddleware} = require('./middleware/parser')
 const {requestsMiddleware} = require('./middleware/externalRequests')
 const {transformerMiddleware} = require('./middleware/transformer')
 const {validateBodyMiddleware} = require('./middleware/validator')
-const {MIDDLEWARE_PATH_REGEX} = require('./constants')
+const {MIDDLEWARE_PATH_REGEX, ALLOWED_HTTP_METHODS} = require('./constants')
 const {populateEndpointCache} = require('./db/services/endpoints/cache')
 
 const middlewareRoute = async router => {
   populateEndpointCache()
 
-  router.post(
-    MIDDLEWARE_PATH_REGEX,
-    initiateContextMiddleware(),
-    parseBodyMiddleware(),
-    requestsMiddleware(),
-    validateBodyMiddleware(),
-    transformerMiddleware(),
-    mapBodyMiddleware()
-  )
-
-  router.get(
-    MIDDLEWARE_PATH_REGEX,
-    initiateContextMiddleware(),
-    parseBodyMiddleware(),
-    requestsMiddleware(),
-    validateBodyMiddleware(),
-    transformerMiddleware(),
-    mapBodyMiddleware()
-  )
+  ALLOWED_HTTP_METHODS.forEach(method => {
+    router[`${method.toLowerCase()}`](
+      MIDDLEWARE_PATH_REGEX,
+      initiateContextMiddleware(),
+      parseBodyMiddleware(),
+      requestsMiddleware(),
+      validateBodyMiddleware(),
+      transformerMiddleware(),
+      mapBodyMiddleware()
+    )
+  })
 }
 
 exports.createMiddlewareRoute = router => {
