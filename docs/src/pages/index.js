a75aed9f7102436eb43e6fa6fb7229e42d83139d
Martin Brocker <martin.brocker@jembi.org>
2020-05-13 10:03:38 +0200

    Updated "getting started" link
    To point to the new Introduction page instead of straight into the setup of the mediator
    
    OHM-1003

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -73,7 +73,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/setup')}>
+              to={useBaseUrl('docs/introduction')}>
               Get Started
             </Link>
           </div>
